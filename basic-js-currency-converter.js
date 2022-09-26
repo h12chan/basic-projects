aafdc7c4f0311d1bc271599493f7dc574646d74b
h12chan <112553752+h12chan@users.noreply.github.com>
2022-09-26 18:39:59 -0400

    Update basic-js-currency-converter.js

--- basic-js-currency-converter.js
+++ basic-js-currency-converter.js
@@ -1,20 +1,26 @@
 const rates = {USDCAD:1.3, GBPUSD:1.09, AUDUSD:0.95}
 
-let usr = ''
+let status = ''
 do {
-	usr = prompt("What is your base currency? (press 'q' to quit)");
-  if (usr == 'q') break
+	status = convertCurrency()
+	console.log(status)
+}
+while (status != 'quit')
+
+function convertCurrency() {
+	let base = prompt("What is your base currency? (press 'q' to quit)");
+  if (base == 'q') return 'quit'
   let quote = prompt("What is your quote currency?");
   let amount = prompt("How much would you like to exchange?");
-  const pair = usr + quote;
+  const pair = base + quote;
   amount = Number(amount)  
-	if (typeof(amount) == "number") {
-  	for (let rate in rates) {
-    	if (rate == pair) {
-      	let total = amount * rates[rate]
-      	console.log(`Converted amount: ${total} ${quote}`)
-      }
+  console.log(`Amount of ${base} to convert is ${amount}`)
+	if (typeof(amount) != "number") return
+ 	for (let rate in rates) {
+   	if (rate == pair) {
+     	let total = amount * rates[rate]
+     	console.log(`Converted amount: ${total} ${quote}`)
+      return 'Successful conversion of currency.'
     }
   }
 }
-while (usr != 'q')
